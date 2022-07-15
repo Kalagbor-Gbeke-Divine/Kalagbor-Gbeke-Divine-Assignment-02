d75c9254d173191e9d49f794513a127010fa2edc
Divine Kalagbor-Gbeke <104471729+Kalagbor-Gbeke-Divine@users.noreply.github.com>
2022-07-15 21:54:28 +0000

    I attempted challenges 1 and 2.

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,13 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let sum = 0;
+  for (let a = 0; a < arrayOfNumbers.length; a++) {
+    sum += arrayOfNumbers[a];
+  }
+  return sum
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +32,15 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let evenNumbers= 0;
+  for(let b = 0; b < arrayOfNumbers.length; b++) {
+    if(arrayOfNumbers[b] % 2 === 0) {
+      evenNumbers++;
+    }
+  }  
+  return evenNumbers
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
@@ -47,9 +57,9 @@ function countEvenNumbers(arrayOfNumbers) {
  * @returns Array the converted temperatures in Fahrenheit
  */
 function celsiusToFahrenheit(arrayOfNumbers) {
-    return arrayOfNumbers;
+  return arrayOfNumbers;
 }
-// assignment.celsiusToFahrenheit = celsiusToFahrenheit;
+//assignment.celsiusToFahrenheit = celsiusToFahrenheit;
 
 
 // ========================
